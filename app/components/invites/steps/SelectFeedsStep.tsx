2fdeab3ede6c4e1063d21f433affa34d09acdfc7
Nolan Picini <nolanpic@gmail.com>
2026-01-28 13:32:59 -0800

    fix skip button

--- app/components/invites/steps/SelectFeedsStep.tsx
+++ app/components/invites/steps/SelectFeedsStep.tsx
@@ -156,6 +156,7 @@ export default function SelectFeedsStep({
           title="Skip"
           titleIcon="arrow-right"
           onClick={handleSkip}
+          className={styles.skipAction}
         ></ActionCard>
       )}
     </div>
